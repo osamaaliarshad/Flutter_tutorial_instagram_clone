2ed6340285be62857269ef7c8fa7aaf6ba161019
oali <oaliarshad@gmail.com>
2021-06-01 16:23:54 -0700

    added comment and post models

--- lib/config/paths.dart
+++ lib/config/paths.dart
@@ -1,4 +1,11 @@
+import 'package:flutter_instagram/models/models.dart';
+
 class Paths {
   // This will have a list of top level collections.
   static const String users = 'users';
+  static const String posts = 'posts';
+  static const String comments = 'comments';
+
+  // Subcollections
+  static const String postComments = 'postComments';
 }
