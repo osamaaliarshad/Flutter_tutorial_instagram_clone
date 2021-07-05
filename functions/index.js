c1a352d1c3667fabebc138c7a518a8e171f97d81
oali <oaliarshad@gmail.com>
2021-07-04 23:31:39 -0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,16 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-exports.onFollowUser = functions.firestore.document('/followers/{userId}/userFollowers/{followerId}').onCreate(async(_, context) => {
+exports.onFollowUser = functions.firestore.document('/followers/{userId}/userFollowers/{followerId}').onCreate(async (_, context) => {
     const userId = context.params.userId;
     const followerId = context.params.followerId;
-    
+
 
     // Increment followed user's followers count
     const followedUserRef = admin.firestore().collection('users').doc(userId);
     const followedUserDoc = await followedUserRef.get();
-    if (followedUserDoc.get('followers') !== undefined){
-        followedUserRef.update({followers: followedUserDoc.get('followers') + 1,});
+    if (followedUserDoc.get('followers') !== undefined) {
+        followedUserRef.update({ followers: followedUserDoc.get('followers') + 1, });
     }
 
     // Increment user's following count. 
